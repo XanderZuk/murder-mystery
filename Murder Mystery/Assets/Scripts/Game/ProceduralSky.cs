9197f015b7bbb5a86c72cdf44bb57606ee6ecfd7
XanderZuk <xander.zuk2@gmail.com>
2020-11-22 21:26:23 -0700

    worked on player animations

---
+++ Murder Mystery/Assets/Scripts/Game/ProceduralSky.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
+
+public class ProceduralSky : MonoBehaviour
+{
+
+    [Range(0, 24)]
+    public float timeOfDay;
+    public Light sun;
+    public Light moon;
+    public Volume skyVolume;
+    public float orbitSpeed = 1.0f;
+    public AnimationCurve starsCurve;
+
+    private bool isNight;
+    private PhysicallyBasedSky sky;
+    // Start is called before the first frame update
+    void Start()
+    {
+        skyVolume.profile.TryGet(out sky);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeOfDay += Time.deltaTime * orbitSpeed;
+        if (timeOfDay > 24)
+        {
+            timeOfDay = 0;
+        }
+    }
+    private void OnValidate()
+    {
+        skyVolume.profile.TryGet(out sky);
+        UpdateTime();
+    }
+
+    private void UpdateTime()
+    {
+        float alpha = timeOfDay / 24.0f;
+        float sunRotation = Mathf.Lerp(-90, 270, alpha);
+        float moonRotation = sunRotation - 180;
+        sun.transform.rotation = Quaternion.Euler(sunRotation, -150.0f, 0);
+        moon.transform.rotation = Quaternion.Euler(moonRotation, -150.0f, 0);
+        sky.spaceEmissionMultiplier.value = starsCurve.Evaluate(alpha) * 1000;
+        CheckNightDayTransition();
+    }
+
+    private void CheckNightDayTransition()
+    {
+        if (isNight)
+        {
+            if (moon.transform.rotation.eulerAngles.x > 180)
+            {
+                StartDay();
+            }
+        }
+        else
+        {
+            if (sun.transform.rotation.eulerAngles.x > 180)
+            {
+                StartNight();
+            }
+        }
+    }
+
+    private void StartDay()
+    {
+        isNight = false;
+        sun.shadows = LightShadows.Soft;
+        moon.shadows = LightShadows.None;
+    }
+
+    private void StartNight()
+    {
+        isNight = true;
+        sun.shadows = LightShadows.None;
+        moon.shadows = LightShadows.Soft;
+    }
+
+}
